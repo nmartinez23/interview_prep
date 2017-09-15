9cba6d25de10c65c163607c90be4765ec8b48fa8
Nick Martinez <23nicholasmartinez@gmail.com>
2017-09-15 08:44:57 -0700

    Create num_types.rb

---
+++ num_types.rb
@@ -0,0 +1,9 @@
+val1 = 10
+val2 = 10.0
+val3 = "10"
+puts "#{val1.inspect} is the type #{val1.class}" # Fixnum
+puts "#{val2.inspect} is the type #{val2.class}" # Float
+puts "#{val3.inspect} is the type #{val3.class}" # String
+Val1.to_f # 10.0
+Val2.to_i # 10
+Val2.to_s # "10" 
