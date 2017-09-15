40dc7128a60754ed25ec6a878e57ea19645247e2
Nick Martinez <23nicholasmartinez@gmail.com>
2017-09-15 10:08:45 -0700

    Create class_ball_object.rb

---
+++ class_ball_object.rb
@@ -0,0 +1,16 @@
+# Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated. 
+# If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+class Ball
+  attr_accessor :ball_type
+  
+  def initialize(ball_type = "regular")
+    @ball_type = ball_type
+  end
+end
+
+ball1 = Ball.new
+ball2 = Ball.new "super"
+
+puts ball1.ball_type  #regular
+puts ball2.ball_type  #super
