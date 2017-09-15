b604123c020da3906f22d9a51618cdb0904fb66a
Nick Martinez <23nicholasmartinez@gmail.com>
2017-09-15 08:47:48 -0700

    Update num_types.rb

--- num_types.rb
+++ num_types.rb
@@ -4,6 +4,6 @@ val3 = "10"
 puts "#{val1.inspect} is the type #{val1.class}" # Fixnum
 puts "#{val2.inspect} is the type #{val2.class}" # Float
 puts "#{val3.inspect} is the type #{val3.class}" # String
-Val1.to_f # 10.0
-Val2.to_i # 10
-Val2.to_s # "10" 
+val1.to_f # 10.0
+val2.to_i # 10
+val2.to_s # "10" 
