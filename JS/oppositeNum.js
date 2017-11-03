06ef5df6709cb3b8d5c9c3278982e018f40c771c
Nick Martinez <23nicholasmartinez@gmail.com>
2017-11-03 11:43:42 -0700

    Add convert num to opposite num

---
+++ JS/oppositeNum.js
@@ -0,0 +1,12 @@
+// Create a function that takes a number and returns the opposite
+// number. If number is positive it should return the same number
+// as negative...and vice versa.
+
+function oppositeNumber(n) {
+  var result = 0 - n;
+
+  return result;
+};
+
+var answer = oppositeNumber(7);
+console.log(answer);
