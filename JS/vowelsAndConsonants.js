ab79671d842cd360eeec48371bd4b231fa4f31a6
Nick Martinez <23nicholasmartinez@gmail.com>
2017-11-04 12:56:38 -0700

    Add find vowels and consonants in a string

---
+++ JS/vowelsAndConsonants.js
@@ -0,0 +1,21 @@
+// Complete the vowelsAndConsonants function.
+// Print your output using 'console.log()'.
+
+function vowelsAndConsonants(s) {
+  let vowels = [];
+  let consonants = [];
+  let str = s.split('');
+
+  str.forEach(function(letter) {
+    if(letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
+      vowels.push(letter);
+      console.log(letter);
+    } else {
+      consonants.push(letter);
+    }
+  });
+
+  consonants.forEach(function(letter) {
+    console.log(letter);
+  });
+}
