04a9ec18e937f4ad6b9bb177a9e38cdbb2feafab
Nick Martinez <23nicholasmartinez@gmail.com>
2017-11-10 09:34:24 -0800

    Update palindrome.js

--- JS/palindrome.js
+++ JS/palindrome.js
@@ -2,18 +2,23 @@
 // if input has space(s)
 function pal(str) {
   var originalStr = str.split('');
+  
   for (var i = 0; i < originalStr.length; i++) {
     if (originalStr[i] === " ") {
       originalStr.splice(i, 1);
     }
   }
-  var reversed = originalStr.reverse().join('');
+
   originalStr = originalStr.join('');
+  var reversed = originalStr.reverse().join('');
+  
   if (originalStr === reversed) {
     return true;
   } 
+  
   return false;
 }
+
 pal("race car");
 
 
@@ -21,9 +26,12 @@ pal("race car");
 // if input has no spaces
 function pal(str) {
   var reversed = str.split('').reverse().join('');
+  
   if (str === reversed) {
     return true;
   } 
+  
   return false;
 }
+
 pal("racecar");
